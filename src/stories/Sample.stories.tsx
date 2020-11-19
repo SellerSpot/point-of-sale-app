81f058d0f47b9f5e468d97b445393e26985e3408
ThayalanGR <grthayalan18@gmail.com>
2020-11-19 14:02:46 +0530

    story book integrated

---
+++ src/stories/Sample.stories.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { Sample } from '../components/Sample/Sample';
+
+export default {
+    title: 'Example/Page',
+    component: Sample,
+} as Meta;
+
+const Template: Story = (args) => <Sample {...args} />;
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
