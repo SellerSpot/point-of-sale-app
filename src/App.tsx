47a90370eb2e6fb8ec24b315105dc7d05561536f
ThayalanGR <grthayalan18@gmail.com>
2020-11-19 08:37:19 +0530

    emotionsjs integration study - ejecting react-app for additional babel integration

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import { Core } from './layouts/Core/Core';
 import React, { FC } from 'react';
+import { Switch, Route } from 'react-router-dom';
+import { ROUTES } from './config/routes';
 
 export const App: FC = (): JSX.Element => {
     return (
         <div>
-            <Core />
+            <Switch>
+                <Route path={ROUTES.HOME}>
+                    <Core />
+                </Route>
+            </Switch>
         </div>
     );
 };
--- src/components/Sample.tsx
+++ src/components/Sample.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
 
 export const Sample: React.FC = (): JSX.Element => {
-    return <div>Sample common component</div>;
+    return (
+        <div>
+            <div>Sample common component</div>
+        </div>
+    );
 };
