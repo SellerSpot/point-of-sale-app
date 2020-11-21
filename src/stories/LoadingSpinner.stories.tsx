70596f484f6f5a8159afe04352e24900a2493308
RohitRajP <RohitRajP>
2020-11-21 18:35:02 +0530

    Added base loading spinner story with component

---
+++ src/stories/LoadingSpinner.stories.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Story, Meta } from '@storybook/react/types-6-0';
+import { LoadingSpinner, PropsType } from '../components/LoadingSpinner/LoadingSpinner';
+
+export default {
+    title: 'Components',
+    component: LoadingSpinner,
+} as Meta;
+
+const Template: Story<PropsType> = (args: PropsType) => <LoadingSpinner {...args} />;
+
+export const LoadingSpinners = Template.bind({});
+LoadingSpinners.args = {
+    size: 'small',
+} as PropsType;
