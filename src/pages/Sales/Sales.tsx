9795d939752f4ba8227a4d5a06a5be7e946ca4d2
RohitRajP <RohitRajP>
2020-11-24 21:05:09 +0530

    Added the TabBar to the sales page

--- src/pages/Sales/Sales.tsx
+++ src/pages/Sales/Sales.tsx
@@ -1,5 +1,19 @@
 import React from 'react';
+import styles from './sales.module.css';
+import { TabBar } from '../../components/TabBar/TabBar';
+
+// used to handle the click on the different tabs
+const tabSelectedCallback = () => {
+    // eslint-disable-next-line no-console
+    // console.log(selectedTab);
+};
 
 export const Sales = (): JSX.Element => {
-    return <div>sales</div>;
+    return (
+        <div>
+            <div className={styles.tabBarHolder}>
+                <TabBar tabs={['Current Sales']} onClickHandler={tabSelectedCallback} selectedTab={0} />
+            </div>
+        </div>
+    );
 };
