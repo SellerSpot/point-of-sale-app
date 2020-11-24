52ec9352d9fdfd650774d9a963cacd5b4822547e
RohitRajP <RohitRajP>
2020-11-24 09:31:14 +0530

    Pulled changes from origin

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import styles from './table.module.css';
 
 export type PropsType = {
     sampleValue: string;
+    headings: string[];
+    values: string[][];
+    tableHeight?: React.CSSProperties;
+    tableWidth?: React.CSSProperties;
+    fullSizedTable?: boolean;
 };
 
 export const Table: React.FC<PropsType> = (): JSX.Element => {
     return (
-        <div>
-            <div>Table Component</div>
+        <div className={styles.table}>
+            <div className={styles.tableHeader}>Table Header</div>
+            <div>Sample</div>
+            <div>Sample</div>
         </div>
     );
 };
