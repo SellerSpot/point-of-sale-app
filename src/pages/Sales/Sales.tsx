95fe7b316b56aaccb09c81410eacd24696ae4348
RohitRajP <RohitRajP>
2020-11-24 21:20:54 +0530

    Added onClick state management to the tabs in the sales page

--- src/pages/Sales/Sales.tsx
+++ src/pages/Sales/Sales.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './sales.module.css';
 import { TabBar } from '../../components/TabBar/TabBar';
 
-// used to handle the click on the different tabs
-const tabSelectedCallback = () => {
-    // eslint-disable-next-line no-console
-    // console.log(selectedTab);
-};
-
 export const Sales = (): JSX.Element => {
+    // to manage which tab is selected
+    const [currTab, setcurrTab] = useState(0);
+
     return (
         <div>
             <div className={styles.tabBarHolder}>
                 <TabBar
                     tabs={['Current Sales', 'More Tabs', 'Even More']}
-                    onClickHandler={tabSelectedCallback}
-                    selectedTab={0}
+                    onClickHandler={setcurrTab}
+                    selectedTab={currTab}
                 />
             </div>
         </div>
