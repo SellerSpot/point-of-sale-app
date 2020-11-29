8973cad7e08caa81b017fa48daefb9ecf453fd8c
RohitRajP <RohitRajP>
2020-11-30 00:00:09 +0530

    Completed table porting to new design spec with tooltip component for the headers

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,32 +1,49 @@
 import React, { ReactNode } from 'react';
 import styles from './table.module.css';
 import { nanoid } from 'nanoid';
+import { cssColors, cssVariables } from '../../config/cssVariables';
+import { HorizontalRule } from '../HorizontalRule/HorizontalRule';
+import ReactTooltip from 'react-tooltip';
 
-export type PropsType = {
-    headings: string[];
-    values: string[][];
+export interface ITableProps {
+    headers: string[];
+    rowData: string[][];
+    style?: React.CSSProperties;
+}
+
+const defaultProps: ITableProps = {
+    headers: ['Heading 1', 'Heading 2', 'Heading 3', 'Heading 4'],
+    rowData: [
+        ['Cell 00', 'Cell 01', 'Cell 02', 'Cell 03'],
+        ['Cell 10', 'Cell 11', 'Cell 12', 'Cell 13'],
+    ],
+    style: {},
 };
 
 // used to generate the table headers
-const tableHeaderFactory = (props: PropsType): ReactNode => {
+const tableHeaderFactory = (sProps: ITableProps): ReactNode => {
     const rowStyle: React.CSSProperties = {
-        gridTemplateColumns: 'repeat(' + props.headings.length + ',1fr)',
+        gridTemplateColumns: 'repeat(' + sProps.headers.length + ',1fr)',
         gridTemplateRows: '1fr',
+        gap: '5px',
         position: 'sticky',
         top: 0,
         width: '100%',
-        height: '40px',
-        fontWeight: 'bold',
+        height: '35px',
+        backgroundColor: cssColors['--tertiary-background-color'],
     };
     const cellStyle: React.CSSProperties = {
-        height: '40px',
+        color: cssColors['--primary-font-color'],
+        fontWeight: 600,
+        fontSize: cssVariables['--font-size-secondary'],
+        height: '35px',
     };
     const classNames = styles.row + ' ' + styles.greyBackground;
     return (
         <div key={nanoid()} className={classNames} style={rowStyle}>
-            {props.headings.map((heading) => {
+            {sProps.headers.map((heading) => {
                 return (
-                    <div key={nanoid()} className={styles.bodyCell} style={cellStyle}>
+                    <div data-tip={heading} key={nanoid()} className={styles.bodyCell} style={cellStyle}>
                         {heading}
                     </div>
                 );
@@ -36,39 +53,54 @@ const tableHeaderFactory = (props: PropsType): ReactNode => {
 };
 
 // used to generate the table body
-const tableRowFactory = (props: PropsType): ReactNode => {
+const tableRowFactory = (sProps: ITableProps): ReactNode => {
     const rowStyle: React.CSSProperties = {
-        gridTemplateColumns: 'repeat(' + props.headings.length + ',1fr)',
+        gridTemplateColumns: 'repeat(' + sProps.headers.length + ',1fr)',
         gridTemplateRows: 'auto',
-        height: '70px',
+        gap: '5px',
     };
-    const cellStyle: React.CSSProperties = {
-        height: '70px',
-    };
-    return props.values.map((row, index) => {
+    const cellStyle: React.CSSProperties = {};
+    return sProps.rowData.map((row, index) => {
         let classNames = styles.row;
         if (index % 2 !== 0) {
             classNames += ' ' + styles.greyBackground;
         }
         return (
-            <div key={nanoid()} className={classNames} style={rowStyle}>
-                {row.map((cell: string) => {
-                    return (
-                        <div key={nanoid()} className={styles.bodyCell} style={cellStyle}>
-                            {cell}
-                        </div>
-                    );
-                })}
+            <div key={nanoid()}>
+                <div className={classNames} style={rowStyle}>
+                    {row.map((cell: string) => {
+                        return (
+                            <div key={nanoid()} className={styles.bodyCell} style={cellStyle}>
+                                {cell}
+                            </div>
+                        );
+                    })}
+                </div>
+                <HorizontalRule
+                    ruleColor={cssColors['--border-color']}
+                    ruleWidth="100%"
+                    style={{ paddingBottom: 0, paddingLeft: 0, paddingRight: 0, paddingTop: 0 }}
+                    alignment="center"
+                />
             </div>
         );
     });
 };
 
-export const Table: React.FC<PropsType> = (props: PropsType): JSX.Element => {
+export const Table: React.FC<ITableProps> = (props: ITableProps): JSX.Element => {
+    // seasoning the sProps
+    const sProps: ITableProps = {
+        ...defaultProps,
+        ...props,
+    };
+
     return (
-        <div className={styles.table}>
-            {tableHeaderFactory(props)}
-            {tableRowFactory(props)}
+        <div>
+            <div className={styles.table} style={sProps.style}>
+                {tableHeaderFactory(sProps)}
+                {tableRowFactory(sProps)}
+            </div>
+            <ReactTooltip effect="solid" />
         </div>
     );
 };
