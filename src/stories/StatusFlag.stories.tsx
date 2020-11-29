d65bfcd360f3c1c5e4084ae77a2614413ba77645
RohitRajP <RohitRajP>
2020-11-29 21:58:43 +0530

    Ported the StatusFlag component to new design spec

--- src/stories/StatusFlag.stories.tsx
+++ src/stories/StatusFlag.stories.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../styles/core.css';
 import { Story, Meta } from '@storybook/react/types-6-0';
-import { StatusFlag, PropsType } from '../components/StatusFlag/StatusFlag';
+import { StatusFlag, IStatusFlagProps } from '../components/StatusFlag/StatusFlag';
 import { loadCSSValues } from '../config/cssVariables';
 
 // used to load css variables in ts object into the :root context
@@ -12,9 +12,9 @@ export default {
     component: StatusFlag,
 } as Meta;
 
-const Template: Story<PropsType> = (args: PropsType) => <StatusFlag {...args} />;
+const Template: Story<IStatusFlagProps> = (args: IStatusFlagProps) => <StatusFlag {...args} />;
 
 export const StatusFlags = Template.bind({});
 StatusFlags.args = {
-    type: 'completed',
-} as PropsType;
+    status: 'completed',
+} as IStatusFlagProps;
