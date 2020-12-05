b568c5061107ed44de3dbf0c04f116a5da5965f1
Rohit Raj <rij7u7d@gmail.com>
2020-12-05 13:15:05 +0530

    Updated SideNav to updated design specs

--- src/config/cssVariables.ts
+++ src/config/cssVariables.ts
@@ -1,5 +1,6 @@
 export const cssColors = {
     /* section colors */
+    '--sellerspot-color': '#172B4D',
     '--sales-color': '#3f51b5',
     '--inventory-color': '#ff5722',
     '--cashregister-color': '#2196f3',
@@ -25,10 +26,10 @@ export const cssColors = {
     /* backgrounds */
     /* card" backgrounds */
     '--primary-background-color': '#ffffff',
-    /* page background, selected" tab */
-    '--secondary-background-color': '#f5f5f5',
-    /* table header" background */
-    '--tertiary-background-color': '#f1f4f9',
+    /* page background */
+    '--secondary-background-color': '#f3f4f6',
+    /* table header" background selected tab */
+    '--tertiary-background-color': '#F5F5F5',
     /* used as overlay backgrounds */
     '--overlay-color': 'rgb(0, 0, 0, 0.5)',
 
