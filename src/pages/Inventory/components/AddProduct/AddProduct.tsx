38cf061840e646c16dadd0cf928ce1b183ab7bc9
Rohit Raj <rij7u7d@gmail.com>
2020-12-19 11:22:34 +0530

    Completed adding validation and error handing for the AddProduct page

--- src/pages/Inventory/components/AddProduct/AddProduct.tsx
+++ src/pages/Inventory/components/AddProduct/AddProduct.tsx
@@ -5,27 +5,46 @@ import { HorizontalRule } from '../../../../components/HorizontalRule/Horizontal
 import { InputField } from '../../../../components/InputField/InputField';
 import styles from './addproduct.module.css';
 import cn from 'classnames';
-import { useDispatch } from 'react-redux';
-import { toggleSliderModal } from '../../../../store/models/sliderModal';
-// import * as Yup from 'yup';
-// import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { useFormik } from 'formik';
 
 export const AddProduct = (): ReactElement => {
-    const dispatch = useDispatch();
+    const formSchema = Yup.object().shape({
+        name: Yup.string().required('Product Name is a required field'),
+        gtin: Yup.string(),
+        category: Yup.string(),
+        brand: Yup.string(),
+        markup: Yup.number(),
+        landingPrice: Yup.number()
+            .min(0, 'Landing Price must be greater than or equal to 0')
+            .required('Landing Price is a required field'),
+        sellingPrice: Yup.number()
+            .min(0, 'Selling Price must be greater than or equal to 0')
+            .required('Selling Price is a required field'),
+        stockLevel: Yup.number().min(0, 'Stock Level must be greater than or equal to 0'),
+        stockUnit: Yup.string(),
+    });
 
-    // const formSchema = Yup.object().shape({
-    //     name: Yup.string().required('Product Name is a required field'),
-    //     gtin: Yup.string(),
-    //     category: Yup.string(),
-    //     brand: Yup.string(),
-    //     markup: Yup.string(),
-    //     sellingPrice: Yup.string().required('Selling Price is a required field'),
-    //     stockLevel: Yup.string(),
-    //     stockUnit: Yup.string(),
-    // });
+    const addProductFormik = useFormik({
+        initialValues: {
+            name: '',
+            gtin: '',
+            category: '',
+            brand: '',
+            markup: 0,
+            landingPrice: 0,
+            sellingPrice: 0,
+            stockLevel: 0,
+            stockUnit: '',
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            alert(JSON.stringify(values, null, 2));
+        },
+    });
 
     return (
-        <form className={cn(styles.addProductWrapper)}>
+        <form onSubmit={addProductFormik.handleSubmit} className={cn(styles.addProductWrapper)} noValidate>
             <div className={styles.addProductHeader}>Add Product</div>
             <div className={styles.addProductBody}>
                 <div className={cn(styles.formGroup)}>
@@ -33,7 +52,13 @@ export const AddProduct = (): ReactElement => {
                         type={'text'}
                         label={'Product Name'}
                         placeHolder={'Product Name'}
-                        onChange={() => void 0}
+                        required={true}
+                        value={addProductFormik.values.name}
+                        error={{
+                            errorMessage: addProductFormik.errors.name ?? '',
+                            showError: addProductFormik.errors.name !== undefined,
+                        }}
+                        onChange={(value) => addProductFormik.setFieldValue('name', value)}
                     />
                 </div>
                 <div className={cn(styles.formGroup)}>
@@ -41,22 +66,27 @@ export const AddProduct = (): ReactElement => {
                         type={'text'}
                         label={'Product GTIN'}
                         placeHolder={'Product Code'}
-                        onChange={() => void 0}
+                        value={addProductFormik.values.gtin}
+                        error={{
+                            errorMessage: addProductFormik.errors.gtin ?? '',
+                            showError: addProductFormik.errors.gtin !== undefined,
+                        }}
+                        onChange={(value) => addProductFormik.setFieldValue('gtin', value)}
                     />
                 </div>
                 <div className={cn(styles.formGroup, styles.formGroupSplitEqual)}>
                     <Dropdown
                         label={'Product Category'}
                         options={['Category One', 'Category Two', 'Category Three']}
-                        onSelect={() => {
-                            // console.log(option);
+                        onSelect={(value) => {
+                            addProductFormik.setFieldValue('category', value);
                         }}
                     />
                     <Dropdown
                         label={'Product Brand'}
                         options={['Brand One', 'Brand Two', 'Brand Three']}
-                        onSelect={() => {
-                            // console.log(option);
+                        onSelect={(value) => {
+                            addProductFormik.setFieldValue('brand', value);
                         }}
                     />
                 </div>
@@ -64,23 +94,40 @@ export const AddProduct = (): ReactElement => {
                 <div className={cn(styles.formGroup, styles.formGroupSplitEqual)}>
                     <InputField
                         type={'number'}
-                        label={'Cost Price'}
-                        placeHolder={'Cost Price'}
-                        onChange={() => void 0}
+                        label={'Landing Price'}
+                        placeHolder={'Landing Price'}
+                        required={true}
+                        value={addProductFormik.values.landingPrice.toString()}
+                        onChange={(value) => addProductFormik.setFieldValue('landingPrice', value)}
+                        error={{
+                            errorMessage: addProductFormik.errors.landingPrice ?? '',
+                            showError: addProductFormik.errors.landingPrice !== undefined,
+                        }}
                     />
                     <InputField
                         type={'number'}
                         label={'Markup %'}
                         placeHolder={'Markup Percent'}
-                        onChange={() => void 0}
+                        value={addProductFormik.values.markup.toString()}
+                        onChange={(value) => addProductFormik.setFieldValue('markup', value)}
+                        error={{
+                            errorMessage: addProductFormik.errors.markup ?? '',
+                            showError: addProductFormik.errors.markup !== undefined,
+                        }}
                     />
                 </div>
                 <div className={cn(styles.formGroup, styles.formGroupSplitEqual)}>
                     <InputField
                         type={'number'}
                         label={'Selling Price'}
+                        required={true}
                         placeHolder={'Selling Price'}
-                        onChange={() => void 0}
+                        value={addProductFormik.values.sellingPrice.toString()}
+                        onChange={(value) => addProductFormik.setFieldValue('sellingPrice', value)}
+                        error={{
+                            errorMessage: addProductFormik.errors.sellingPrice ?? '',
+                            showError: addProductFormik.errors.sellingPrice !== undefined,
+                        }}
                     />
                 </div>
                 <HorizontalRule style={{ paddingTop: 10, paddingBottom: 30 }} />
@@ -89,34 +136,38 @@ export const AddProduct = (): ReactElement => {
                         type={'number'}
                         label={'Stock Level'}
                         placeHolder={'Stock Level'}
-                        onChange={() => void 0}
+                        value={addProductFormik.values.stockLevel.toString()}
+                        onChange={(value) => addProductFormik.setFieldValue('stockLevel', value)}
+                        error={{
+                            errorMessage: addProductFormik.errors.stockLevel ?? '',
+                            showError: addProductFormik.errors.stockLevel !== undefined,
+                        }}
                     />
-                    <InputField
-                        type={'number'}
+                    <Dropdown
                         label={'Stock Unit'}
-                        placeHolder={'Stock Unit'}
-                        onChange={() => void 0}
+                        options={['KG', 'Pieces', 'Liters']}
+                        onSelect={(value) => {
+                            addProductFormik.setFieldValue('stockUnit', value);
+                        }}
                     />
                 </div>
             </div>
             <div className={styles.addProductFooter}>
                 <Button
-                    type="button"
+                    type="submit"
                     shape="rectangle"
-                    label="Cancel"
+                    label="Add Product"
                     variant="solid"
-                    backgroundColor="--danger-color"
+                    backgroundColor="--inventory-color"
                     labelColor="--light-font-color"
-                    style={{ marginRight: 'auto' }}
-                    onClick={() => dispatch(toggleSliderModal({ sliderName: 'addProductSlider', active: false }))}
                 />
                 <Button
                     type="button"
                     shape="rectangle"
-                    label="Add Product"
-                    variant="solid"
-                    backgroundColor="--warning-color"
-                    labelColor="--light-font-color"
+                    label="Reset Values"
+                    variant="outline"
+                    backgroundColor="--inventory-color"
+                    labelColor="--inventory-color"
                 />
             </div>
         </form>
