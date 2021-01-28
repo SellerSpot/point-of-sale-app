aa6b299594186c2c6f600639f459b0b2df3bebea
Rohit Raj <rij7u7d@gmail.com>
2021-01-29 00:33:18 +0530

    Complete import of slider component

--- src/layouts/Dashboard/components/Sliders/Sliders.tsx
+++ src/layouts/Dashboard/components/Sliders/Sliders.tsx
@@ -1,9 +1,9 @@
 import { SliderModal } from '@sellerspot/universal-components';
-import { AddBrand } from 'pages/Inventory/components/AddBrand/AddBrand';
-import { AddCategory } from 'pages/Inventory/components/AddCategory/AddCategory';
-import { AddProduct } from 'pages/Inventory/components/AddProduct/AddProduct';
-import { AddTaxBracket } from 'pages/Inventory/components/AddTaxBracket/AddTaxBracket';
-import { NewSale } from 'pages/Sale/components/NewSale/NewSale';
+// import { AddBrand } from 'pages/Inventory/components/AddBrand/AddBrand';
+// import { AddCategory } from 'pages/Inventory/components/AddCategory/AddCategory';
+// import { AddProduct } from 'pages/Inventory/components/AddProduct/AddProduct';
+// import { AddTaxBracket } from 'pages/Inventory/components/AddTaxBracket/AddTaxBracket';
+// import { NewSale } from 'pages/Sale/components/NewSale/NewSale';
 import React, { ReactElement, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { sliderModalSelector } from 'store/models/sliderModal';
@@ -23,47 +23,47 @@ const Sliders = (): ReactElement => {
 
     return (
         <>
-            <SliderModal
+            {/* <SliderModal
                 active={newSaleSlider.show}
                 sliderSize={'100%'}
                 onClickBackdrop={() => callBackStateTrack[1](true)}
                 onClickEsc={() => callBackStateTrack[1](true)}
             >
                 <NewSale />
-            </SliderModal>
-            <SliderModal
+            </SliderModal> */}
+            {/* <SliderModal
                 active={addProductSlider.show}
                 sliderSize={'50%'}
                 onClickBackdrop={() => callBackStateTrack[1](true)}
                 onClickEsc={() => callBackStateTrack[1](true)}
             >
                 <AddProduct callBackStateTrack={callBackStateTrack} />
-            </SliderModal>
-            <SliderModal
+            </SliderModal> */}
+            {/* <SliderModal
                 active={addBrandSlider.show}
                 sliderSize={'30%'}
                 onClickBackdrop={() => callBackStateTrack[1](true)}
                 onClickEsc={() => callBackStateTrack[1](true)}
             >
                 <AddBrand callBackStateTrack={callBackStateTrack} />
-            </SliderModal>
+            </SliderModal> */}
 
-            <SliderModal
+            {/* <SliderModal
                 active={addCategorySlider.show}
                 sliderSize={'30%'}
                 onClickBackdrop={() => callBackStateTrack[1](true)}
                 onClickEsc={() => callBackStateTrack[1](true)}
             >
                 <AddCategory callBackStateTrack={callBackStateTrack} />
-            </SliderModal>
-            <SliderModal
+            </SliderModal> */}
+            {/* <SliderModal
                 active={addTaxBracketSlider.show}
                 sliderSize={'30%'}
                 onClickBackdrop={() => callBackStateTrack[1](true)}
                 onClickEsc={() => callBackStateTrack[1](true)}
             >
                 <AddTaxBracket callBackStateTrack={callBackStateTrack} />
-            </SliderModal>
+            </SliderModal> */}
 
             {/* <SliderModal
                 active={checkoutSlider.show}
