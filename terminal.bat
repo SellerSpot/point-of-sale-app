4b2d5f3c903097ca11b677865924210727300c84
Rohit Raj <rij7u7d@gmail.com>
2021-01-30 20:42:02 +0530

    Update terminal

--- terminal.bat
+++ terminal.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 wt -M nt -d ..\point-of-sale-server ; sp -V -d ..\point-of-sale-app 
-=======
-wt -M nt -d ..\point-of-sale-server ; sp -V -d ..\point-of-sale-app ; sp -V -d ..\database-models ; sp -H -d ..\universal-types ; sp -H -d ..\universal-components
->>>>>>> 8f0d1b309a0219f749edf8b2d30041cc47e5a1f4
