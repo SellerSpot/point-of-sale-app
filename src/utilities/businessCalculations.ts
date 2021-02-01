d5ac367394483ed653490a89d51e8596b07a9c59
Rohit Raj <rij7u7d@gmail.com>
2021-02-01 08:08:36 +0530

    Add Business calculations for computing subtotal

---
+++ src/utilities/businessCalculations.ts
@@ -0,0 +1,43 @@
+import { isUndefined } from 'lodash';
+
+interface IXPercentOfY {
+    x: number;
+    y: number;
+}
+/**
+ * Computes what x% of y is
+ * @param x percentage
+ * @param y value to find percentage of
+ */
+export const xPercentOfY = (props: IXPercentOfY) => (props.x / 100) * props.y;
+
+interface IComputeSubtotal {
+    itemPrice: number;
+    taxPercents?: number[];
+    discount?: {
+        percent?: number;
+        value?: number;
+    };
+}
+/**
+ * Compute subtotal
+ */
+export const computeSubtotal = (props: IComputeSubtotal): number => {
+    // holds the final subtotal value
+    let subTotal: number = props.itemPrice;
+    // tax calculations
+    if (!isUndefined(props.taxPercents)) {
+        props.taxPercents.map(
+            (taxPercent) => (subTotal += xPercentOfY({ x: taxPercent, y: props.itemPrice })),
+        );
+    }
+    // discount calculations
+    if (!isUndefined(props.discount)) {
+        if (!isUndefined(props.discount.percent)) {
+            subTotal += xPercentOfY({ x: props.discount.percent, y: props.itemPrice });
+        } else if (!isUndefined(props.discount.value)) {
+            subTotal -= props.discount.value;
+        }
+    }
+    return subTotal;
+};
