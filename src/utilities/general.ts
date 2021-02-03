eeeaac5401069e2ba6da03d300d442bf3659069a
Rohit Raj <rij7u7d@gmail.com>
2021-02-03 20:36:50 +0530

    Moved Addx shortcuts from Alt to Shift

--- src/utilities/general.ts
+++ src/utilities/general.ts
@@ -32,9 +32,9 @@ export const introduceDelay = async (delay = 4000): Promise<boolean> =>
  */
 export const GLOBAL_KEYBOARD_SHORTCUTS = {
     NEW_SALE: 'F1',
-    ADD_PRODUCT: 'ALT+P',
-    ADD_CATEGORY: 'ALT+C',
-    ADD_BRAND: 'ALT+B',
-    ADD_TAXBRACKET: 'ALT+T',
-    ADD_STOCKUNIT: 'ALT+S',
+    ADD_PRODUCT: 'SHIFT+P',
+    ADD_CATEGORY: 'SHIFT+C',
+    ADD_BRAND: 'SHIFT+B',
+    ADD_TAXBRACKET: 'SHIFT+T',
+    ADD_STOCKUNIT: 'SHIFT+S',
 };
