a86303cff0bed553e814136f494d73b4b5a86e5b
Rohit Raj <rij7u7d@gmail.com>
2021-02-07 02:57:28 +0530

    Created CartItemDetail Page

---
+++ src/pages/Sale/components/NewSale/components/CartItemDetail/CartItemDetail.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import styles from './cartItemDetail.module.scss';
+
+export const CartItemDetail = (): JSX.Element => {
+    return <div className={styles.pageWrapper}>Sample Content</div>;
+};
